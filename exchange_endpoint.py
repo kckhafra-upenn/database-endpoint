5168e5e6f20c1285bc8621698e2296e8020c7fed
kckhafra <kckhafra@gmail.com>
2021-12-05 14:48:29 -0500

    initial commit

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, g
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine, select, MetaData, Table
+from sqlalchemy import create_engine
 from flask import jsonify
 import json
 import eth_account
@@ -8,9 +8,9 @@ import algosdk
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import load_only
-import hashlib, secrets
+from datetime import datetime
+import sys
 
-import hashlib, secrets
 from models import Base, Order, Log
 engine = create_engine('sqlite:///orders.db')
 Base.metadata.bind = engine
@@ -18,52 +18,43 @@ DBSession = sessionmaker(bind=engine)
 
 app = Flask(__name__)
 
-#These decorators allow you to use g.session to access the database inside the request code
 @app.before_request
 def create_session():
-    g.session = scoped_session(DBSession) #g is an "application global" https://flask.palletsprojects.com/en/1.1.x/api/#application-globals
+    g.session = scoped_session(DBSession)
 
 @app.teardown_appcontext
 def shutdown_session(response_or_exc):
+    sys.stdout.flush()
     g.session.commit()
     g.session.remove()
 
-"""
--------- Helper methods (feel free to add your own!) -------
-"""
-def sha3_256Hash(msg):
-    hashBytes = hashlib.sha3_256(msg.encode("utf8")).digest()
-    return int.from_bytes(hashBytes, byteorder="big")
 
-# def signECDSAsecp256k1(msg, privKey):
-#     msgHash = sha3_256Hash(msg)
-#     signature = sign(generator_secp256k1, privKey, msgHash)
-#     return signature
+""" Suggested helper methods """
 
-def verifyECDSAsecp256k1(msg, signature, pubKey):
-    msgHash = sha3_256Hash(msg)
-    valid = verify(generator_secp256k1, pubKey, msgHash, signature)
-    return valid
+def check_sig(payload,sig):
+    pass
 
+def fill_order(order,txes=[]):
+    pass
+  
 def log_message(d):
-    payload = json.dumps(d['payload'])
-    log_obj = Log(message=payload)
-    g.session.add(log_obj)
-    g.session.commit()
+    # Takes input dictionary d and writes it to the Log table
+    # Hint: use json.dumps or str() to get it in a nice string form
+    pass
+
+""" End of helper methods """
 
 
-"""
----------------- Endpoints ----------------
-"""
-    
+
 @app.route('/trade', methods=['POST'])
 def trade():
+    print("In trade endpoint")
     if request.method == "POST":
         content = request.get_json(silent=True)
         print( f"content = {json.dumps(content)}" )
         columns = [ "sender_pk", "receiver_pk", "buy_currency", "sell_currency", "buy_amount", "sell_amount", "platform" ]
         fields = [ "sig", "payload" ]
-        error = False
+
         for field in fields:
             if not field in content.keys():
                 print( f"{field} not received by Trade" )
@@ -71,29 +62,30 @@ def trade():
                 log_message(content)
                 return jsonify( False )
         
-        error = False
         for column in columns:
             if not column in content['payload'].keys():
                 print( f"{column} not received by Trade" )
-                error = True
-        if error:
-            print( json.dumps(content) )
-            log_message(content)
-            return jsonify( False )
+                print( json.dumps(content) )
+                log_message(content)
+                return jsonify( False )
             
         #Your code here
         #Note that you can access the database session using g.session
-        # if(content['payload']['platform']=="Ethereum"):
-        #     sha3_256Hash(content)
-            
-        return {}
+
+        # TODO: Check the signature
+        
+        # TODO: Add the order to the database
+        
+        # TODO: Fill the order
+        
+        # TODO: Be sure to return jsonify(True) or jsonify(False) depending on if the method was successful
+        
 
 @app.route('/order_book')
 def order_book():
-    result = {"data": g.session.query(Order).all()}
+    #Your code here
+    #Note that you can access the database session using g.session
     return jsonify(result)
-
 
 if __name__ == '__main__':
     app.run(port='5002')
-
